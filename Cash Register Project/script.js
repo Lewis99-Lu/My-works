20c7e9e891e2b89d61e03854ea3a3419e191b6b9
Lewis <intizuuu@gmail.com>
2025-05-17 09:25:44 +0800

    完成Cash Register Project功能

--- Cash Register Project/script.js
+++ Cash Register Project/script.js
@@ -1,5 +1,5 @@
 let price = 19.5;
-let cid = [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
+let cid = [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
 const currencies = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
 const currenciesName = ['Pennies', 'Nickels', 'Dimes', 'Quarters', 'Ones', 'Fives', 'Tens', 'Twenties', 'Hundreds']
 const cash = document.getElementById('cash')
@@ -35,16 +35,18 @@ class Register {
   cashGreaterPrice(customCash){
     let change = (Math.round(customCash * 100) - Math.round(this.itemPrice * 100))/100
     let totalDrawerCash = this.drawerCash.reduce((total,[, cash,]) => total + cash,0)
+    console.log(totalDrawerCash,"@@@")
+    console.log(change,"@@@")
     //收銀機的錢不夠找
     if(totalDrawerCash < change){
       this.cidLessChange()
     }
     //收銀機的錢剛好等於找的錢
     if(totalDrawerCash === change){
-      let sameValueIndex = -1
+      let sameValueIndex = this.drawerCash.findIndex((cash) => cash[1] === change)
       //如果有剛好相同幣值
       if(sameValueIndex !== -1){
-        this.cidEqualChange(sameValueIndex)
+        this.cidEqualChange(sameValueIndex,change)
       }
       //不同幣值
       else{
@@ -57,9 +59,11 @@ class Register {
     }
   }
 
-  cidEqualChange(sameValueIndex){
+  cidEqualChange(sameValueIndex,change){
     changeDue.style.display = 'block'
     changeDue.innerHTML = `Status: CLOSED ${this.drawerCash[sameValueIndex][0]}: $${this.drawerCash[sameValueIndex][1]}`
+    this.drawerCash[sameValueIndex][1] = (Math.round(this.drawerCash[sameValueIndex][1] * 100) - Math.round(change * 100)) / 100
+    this.updateCid()
   }
 
   cidLessChange(){
@@ -73,34 +77,32 @@ class Register {
     for(let cash of drawerCashReverse){
       let [cashName, cashTotal, cashValue] = cash
       let originCashIndex = this.drawerCash.findIndex((cash) => cash[0] === cashName)
+      console.log(originCashIndex)
+      if(drawerCashReverse.indexOf(cash) === drawerCashReverse.length -1 && cashTotal < change){
+        this.cidLessChange()
+        this.updateCid()
+        return
+      }
       // //如果當前幣值小於等於找錢以及當前現金總值還有現金，才用當前幣值找錢
       //未完成需修改
-      while(change >= cashValue && cashTotal > 0){
+      while(change >= cashValue && cashTotal > 0 ){
         let exitCash = changeArr.findIndex((changeCash) => changeCash[0] === cashName)
-        if(exitCash === -1 && cashTotal > change){
-          alert(1)
+        if(exitCash === -1){
           changeArr.push([cashName, cashValue])
-          // cashTotal = Math.round((cashTotal - cashValue) * 100) / 100;
-          // change = (Math.round(change * 100) - Math.round(cashValue * 100)) / 100
-          // this.drawerCash[originCashIndex][1] = (Math.round(this.drawerCash[originCashIndex][1] * 100) - Math.round(cashValue * 100)) / 100
+          change = (Math.round(change * 100) - Math.round(cashValue * 100)) / 100
+          cashTotal = Math.round((cashTotal - cashValue) * 100) / 100;
+          this.drawerCash[originCashIndex][1] = (Math.round(this.drawerCash[originCashIndex][1] * 100) - Math.round(cashValue * 100)) / 100
         }
-        if(exitCash >= 0){
-          alert(2)
+        if(exitCash !== -1){
           changeArr[exitCash][1] = (Math.round(changeArr[exitCash][1] * 100) + Math.round(cashValue * 100)) / 100
-          // cashTotal = Math.round((cashTotal - cashValue) * 100) / 100;
-          // change = (Math.round(change * 100) - Math.round(cashValue * 100)) / 100
-          // this.drawerCash[originCashIndex][1] = (Math.round(this.drawerCash[originCashIndex][1] * 100) - Math.round(cashValue * 100)) / 100
+          change = (Math.round(change * 100) - Math.round(cashValue * 100)) / 100
+          cashTotal = Math.round((cashTotal - cashValue) * 100) / 100;
+          this.drawerCash[originCashIndex][1] = (Math.round(this.drawerCash[originCashIndex][1] * 100) - Math.round(cashValue * 100)) / 100
         }
-        console.log(cashTotal ,"抽屜裡的錢原來")
-        console.log(change,"原本換錢")
+        
         console.log(cashTotal ,"抽屜裡的錢後來")
         console.log(change,"後來換錢")
       }
-    }
-    if(change !== 0){
-      this.cidLessChange()
-      this.updateCid()
-      return
     }
     this.updateCid()
     changeDue.style.display = 'block'
@@ -121,7 +123,7 @@ class Register {
   checkPrice(){  
     //在class內獲取cash的值，每次執行checkPrice會更新cash的值
     let customCash = parseFloat(this.customCash.value)
-    
+    let itemPrice = parseFloat(this.itemPrice)
     if(customCash < this.itemPrice){
       this.cashLessPrice()
       return
